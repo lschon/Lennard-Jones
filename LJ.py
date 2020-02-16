90ecd5597f372ea5890ad8490636106f92f08eab
Lucas Schön <s1718492@ed.ac.uk>
2020-02-16 19:31:08 +0000

    first commit

---
+++ LJ.py
@@ -0,0 +1,142 @@
+"""
+Lennard Jones (LJ) by Lucas Schön (s1718492). February 2020.
+Lennard Jones Project Computer Modelling.
+
+Velocity Verlet time integration of two bonded particles,
+with the aim of determining
+vibrational frequency of the bond.
+
+Produces plots of the position of the particles
+and the energy, both as function of time. Also
+saves both to file.
+
+Uses the LJ potential (and by extension,
+force) with user-provided parameters of XXX
+"""
+
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as pyplot
+from Particle3D import Particle3D
+
+def LJforce(rsep_red, r_red):
+    """
+    Returns force acting on particle as Numpy array
+    """
+    force=48*(1/r_red**(14)-1/(2*r_red**8))*rsep_red
+
+
+    return force
+
+def LJpot(r_red):
+    """
+    Returns potential energy of particle as float
+    """
+
+    pot=4*(1/r_red**(12)-1/r_red**6)
+
+    return pot
+
+# Begin main code
+def main():
+    # Read name of output file from command line
+    if len(sys.argv)!=2:
+        print("Wrong number of arguments.")
+        print("Usage: " + sys.argv[0] + " <output file>")
+        quit()
+    else:
+        outfile_name = sys.argv[1]
+
+    # Open file with parameters
+    parameterfile=open("LJparameters.txt", "r")
+    list=Particle3D.parameterfilereader(parameterfile)
+    e_k_b=float(list[0])
+    sigma=float(list[1])
+    m=float(list[2])
+    epsilon=e_k_b*(1.38064852*10**(-23))
+
+    # Write out initial conditions
+    rsep=Particle3D.separation(p1,p2)
+    r = np.linalg.norm(rsep)
+
+    # Define reduced units
+    r_red=r/sigma
+    rsep_red=rsep/sigma
+    E_red=E/epsilon
+    T_red=e_k_b
+    t_red=sigma*sqrt(m/epsilon)
+
+    # Open output file
+    outfile = open(outfile_name, "w")
+
+    # Set up simulation parameters
+    dt = 0.078
+    time = 0.0
+
+
+    #Define initial force
+    energy = p1.KE() + p2.KE() + LJpot(r)
+    outfile.write("{0:f} {1:f} {2:12.8f}\n".format(time,r,energy))
+
+    # Define initial force
+    force = LJforce(rsep_red, r_red)
+
+    # Initialise data lists for plotting later
+    time_list = [time]
+    pos_list = [r_red]
+    energy_list = [energy]
+
+
+    # Start the time integration loop
+    while time<=20:
+
+        # Update particle position
+        p1.leap_pos_2nd(dt, force)
+        p2.leap_pos_2nd(dt, -force)
+
+        # Update force
+        force_new = LJforce(rsep_red, r_red)
+
+        # Update particle velocity by averaging current and new forces
+        p1.leap_velocity(dt, 0.5*(force+force_new))
+        p2.leap_velocity(dt, -0.5*(force+force_new))
+
+        # Re-define separation ater change in position
+        r_red = Particle3D.separation(p1,p2)
+
+        # Re-define force value
+        force = force_new
+
+        # Increase time
+        time += dt
+
+        # Output particle information
+        energy = p1.KE() +p2.KE() + LJforce(rsep_red, r_red)
+        outfile.write("{0:f} {1:f} {2:12.8f}\n".format(time, r_red, energy))
+
+        # Append information to data lists
+        time_list.append(time)
+        pos_list.append(r_red)
+        energy_list.append(energy)
+
+
+    # Plot particle separation to screen
+    pyplot.title('Velocity Verlet: Separation vs time')
+    pyplot.xlabel('Time (10.18 fs)')
+    pyplot.ylabel('Separation (Å)')
+    pyplot.plot(time_list, pos_list, label="particle separation")
+    pyplot.legend(loc="upper left")
+    pyplot.show()
+
+    # Plot particle energy to screen
+    pyplot.title('Velocity Verlet: total energy vs time')
+    pyplot.xlabel('Time (10.18 fs)')
+    pyplot.ylabel('Energy (eV)')
+    pyplot.plot(time_list, energy_list, label="particle energy")
+    pyplot.legend(loc="upper left")
+    pyplot.show()
+
+# Execute main method, but only when directly invoked
+if __name__ == "__main__":
+    main()
---
+++ Particle3D.py
@@ -0,0 +1,79 @@
+"""PARTICLE 3D by Lucas Schön (s1718492). 28 November 2019. Exercise 2 Computer Modelling.
+Aim: to create a class which provides the position, velocity, separation, and energy details
+of a 3D particle given text files that hold the initial conditions. This code is to be referenced
+in another time integration program and will make use of the functions provided here at each time step"""
+
+import numpy as np
+import csv
+
+
+def NParticles():
+    particles=[]
+    N=5
+    for i in range(N):
+        particles.append([])
+        number=str(i)
+        label="Particle"+number
+        particles[i].extend([0, 0, 0, 0, 0, 0, 1, label])
+    return particles
+NParticles()
+
+
+class Particle3D(object):
+
+    def __init__(self, x, y, z, vx, vy, vz, mass, label):
+        "Initialise a Particle3D instance"
+        "param x,y,z: floats of positions in x, y, and z directions combined into numpy array"
+        "param vx,vy,vz: floats of velocity in x, y, and z directions combined into numpy array"
+        "param mass: mass as float"
+        "param label: label as string"
+        self.position=np.array([x, y, z])
+        self.velocity=np.array([vx, vy, vz])
+        self.mass=float(mass)
+        self.label=str(label)
+
+    def __str__(self):
+        "Define output format."
+        return self.label + "Position: " + str(self.position) + "Velocity: "+ str(self.velocity) + "Mass: " + str(self.mass)
+
+    def KE(self):
+        "Return kinetic energy as 1/2*mass*vel^2"
+        return 0.5*self.mass*(np.sum(np.square(self.velocity)))
+
+    def leap_velocity(self, dt, force):
+        "First-order velocity update, v(t+dt) = v(t) + dt*F(t)"
+        "param dt: timestep as float"
+        "param force: force on particle as float"
+        self.velocity += dt*force/self.mass
+
+    def leap_pos_1st(self, dt):
+        "First-order position update, x(t+dt) = x(t) + dt*v(t)"
+        ":param dt: timestep as float"
+        self.position += dt*self.velocity
+
+    def leap_pos_2nd(self, dt, force):
+        "Second-order position update, x(t+dt) = x(t) + dt*v(t) + (1/2m)*dt^2*F(t)"
+        "param dt: timestep as float"
+        "param force: current force as float"
+        self.position += dt*self.velocity + 0.5*dt**2*force/self.mass
+
+    @staticmethod
+    def filereader(inputfile):
+        """Static method for reading in a text file including coordinates, velocity,
+        mass, and label, and returning a 3D particle by calling the Particle3D function"""
+        line=inputfile.readline()
+        tokens=line.split(",")
+        x=float(tokens[0])
+        y=float(tokens[1])
+        z=float(tokens[2])
+        vx=float(tokens[3])
+        vy=float(tokens[4])
+        vz=float(tokens[5])
+        mass=float(tokens[6])
+        label=(tokens[7])
+        return Particle3D(x,y,z,vx,vy,vz,mass,label)
+
+    @staticmethod
+    def separation(p1,p2):
+        """Static method for determining the separation between two particles"""
+        return (p1.position-p2.position)
